317f348a14a9b14957e922a8a30e4ce41efd3df3
Abderrahmane <153015702+aetherab11@users.noreply.github.com>
2025-05-22 17:39:18 +0100

    backbutton for the timer page

--- js/timer.js
+++ js/timer.js
@@ -674,3 +674,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.getElementById('backButton').addEventListener('click', function() {
+    history.back();
+});
